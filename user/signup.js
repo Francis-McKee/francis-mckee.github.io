52681c90eda0c1e7c27d96441abbe1a41dd4320e
Francis McKee <frankmckee1203@gmail.com>
2025-01-17 00:11:47 -0500

    imported create account and login firebase functs

--- user/signup.js
+++ user/signup.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
 
 // Import Firebase libraries
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
